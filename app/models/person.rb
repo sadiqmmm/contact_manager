ee5e192374a452c05df2f46a9b1ea85434e56f40
Mohammed Sadiq <sadiqmmm@gmail.com>
2014-06-18 07:14:05 +0530

    Add validation on person

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,3 @@
 class Person < ActiveRecord::Base
+	validates :first_name, presence: true
 end
--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,5 +1,7 @@
-require 'rails_helper'
+require 'spec_helper'
 
-RSpec.describe Person, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Person do
+  it 'is valid' do
+    expect(Person.new).to be_valid
+  end
 end
